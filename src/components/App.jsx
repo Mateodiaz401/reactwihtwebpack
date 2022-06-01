caa7445173b24e74f14d6af500a3269ead9e2510
Mateo Díaz <mateodiaz401@gmail.com>
2022-05-31 23:35:27 -0500

    Mandar a pruduccion de prueba de la aplicacion

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const App = () => {
+    return (
+        <h1>Hola mateo Diaz lopez</h1>
+    )
+}
+
+export default App;
---
+++ webpack.config.dev.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    mode: 'development',
+    // entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [{ loader: 'html-loader' }]
+            },
+            {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin(
+            {
+                filename: '[name].css'
+            }
+        ),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 4010
+    }
+}
